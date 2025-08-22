0a08ca6a913960e9c8399bb65dc7a19bcc534836
luthfiadilal <luthfiadilal01@gmail.com>
2025-08-22 13:43:31 +0700

    push repo

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+module.exports = app;
