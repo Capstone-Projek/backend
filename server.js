6a506b5837df684b0577b26809860f495327a986
Luthfi Adilal Mahbub <luthfiadilal01@gmail.com>
2025-08-23 16:03:40 +0700

    feat koneksi DB

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const app = require("./src/app");
-
-require("./src/database");
+require("./src/config/database");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false, // Set to true if you want to see SQL queries in the console
+  }
+);
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connection Database has been established successfully.");
+  } catch (error) {
+    console.error("❌ Unable to connect to DB2:", error);
+  }
+}
+
+testConnection();
+
+module.exports = sequelize;
